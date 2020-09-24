f7446b0b74c1f4d9dd7cbb4f7e9ed957ed6a9cf7
Junghun Seo <sjh7893@gmail.com>
2020-09-24 22:35:33 +0900

    feat: 테스트 코드 추가

--- BitriseTestTests/BitriseTestTests.swift
+++ BitriseTestTests/BitriseTestTests.swift
@@ -30,5 +30,9 @@ class BitriseTestTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func testSuccess() {
+        XCTAssertTrue(true)
+    }
 
 }
